fd387c986400469142acfb503660cdf3044a9d52
Alexandr Voronovich <voronovicha@icloud.com>
2021-07-31 12:39:11 +0300

    Сделал админ панель и бд админов

---
+++ client/js/index/form.js
@@ -0,0 +1,8 @@
+function OpenForm(){
+    const el = document.getElementById('form')
+    el.style.display = 'block'
+}
+function CloseForm(){
+    const el = document.getElementById('form')
+    el.style.display = 'none'
+}
