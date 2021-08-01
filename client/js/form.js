c6ead34191c19cd868d18981f44baa0f23dff44d
Alexandr Voronovich <voronovicha@icloud.com>
2021-08-01 12:59:32 +0300

    Разделил главную страницу, добавил карту и видео

---
+++ client/js/form.js
@@ -0,0 +1,21 @@
+function OpenForm(){
+    const el = document.getElementById('form')
+    el.style.display = 'block'
+}
+function CloseForm(){
+    const el = document.getElementById('form')
+    el.style.display = 'none'
+}
+function SendForm(){
+    const name = document.getElementById('name').value;
+    const phone = document.getElementById('phone').value;
+    let content = '';
+    for (let i = 0; i < localStorage.length - 1; i++) {
+        content += `${localStorage.getItem('' + i)},`
+    }
+    fetch(`./api/create_user/${name}/${phone}/${content}`);
+    localStorage.clear()
+    BasketContent();
+    CloseForm()
+
+}
