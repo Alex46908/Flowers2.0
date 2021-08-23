2616fc37d5341c1aa6eafdfd03da46313f295e4f
Alex46908 <voronovicha@icloud.com>
2021-08-23 22:44:07 +0300

    Updated README

--- ts/telegram.ts
+++ ts/telegram.ts
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'token';
+const token = 'Token';
 const bot = new TelegramBot(token, { polling: true });
 export function Send(full_name:string, phone:string, content:string){
     bot.sendMessage('groupid', `Фио: ${full_name}, Телефон: ${phone}, Заказ: ${content}`);
